ffeb494ca88dfbafe02b02906e5f6a826ebfaa9b
ThatKerbal <nathan.w.baugher@gmail.com>
2026-09-21 18:35:35 -0500

    Removed more unused imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/IntakeCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/IntakeCode.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class IntakeCode {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/Shooter_Transfer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/Shooter_Transfer.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 public class Shooter_Transfer {
 
